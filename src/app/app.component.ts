7d322b97d39c7f74f59b00720e991c8dba747054
Augusto Morelli <a.morelli@sistemas-utnfra.com.ar>
2024-08-28 20:41:38 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { MensajeComponent } from './componentes/mensaje/mensaje.component';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule, MensajeComponent, NgIf, NgFor], //Aca importamos tanto los componentes como los modulos que necesitemos utilizar en este componente
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'clase1';
+ numero1: number = 0;
+ numero2: number = 0;
+ resultado!: number;
+ items = [
+  'Item 1',
+  'Item 2',
+  'Item 3',
+  'Item 4',
+ ]
+
+ sumar() {
+  this.resultado = this.numero1 + this.numero2;
+ }
 }
